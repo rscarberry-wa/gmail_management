0f568f0358ebfc216961001cd83f06ff234d4310
Randall Scarberry <drrandys@yahoo.com>
2026-04-23 14:19:41 -0400

    Add `gmail` tools module with basic email and calendar functionalities

---
+++ src/gmail_assistant/tools/gmail/__init__.py
@@ -0,0 +1,9 @@
+"""Gmail Assistant Tools"""
+
+from gmail_assistant.tools.gmail.gmail_tools import (
+    fetch_emails_tool,
+    reply_email_tool,
+    send_new_email_tool,
+    check_calendar_tool,
+    schedule_meeting_tool
+)
